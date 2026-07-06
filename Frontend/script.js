5d5a20150cfa40270eaf3bdb05f47fcc6ac0dfb5
Andy-Campo <andycampoy1205@gmail.com>
2026-07-06 08:39:27 -0500

    Reestructuracion del Frontend

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,5 +1,7 @@
 // frontend/script.js o dashboard.js
 const API_URL = 'http://localhost:5000/api';
+// Asegúrate de usar la URL de tu Web Service de Render con el prefijo /api
+const BACKEND_URL = 'https://sandwatch-backend.onrender.com/api';
 
 // Ejemplo de petición Fetch para registrar o validar
 async function obtenerHijos() {
