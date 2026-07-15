2c0047db49e1d64bf098c6b12d59163a91c4db92
Andy-Campo <andycampoy1205@gmail.com>
2026-07-14 19:44:04 -0500

    animaciones login

---
+++ Frontend/js/auth-ui.js
@@ -0,0 +1,23 @@
+const signIn = document.getElementById('sign-in');
+const signUp = document.getElementById('sign-up');
+const form = document.getElementById('form');
+const banner = document.getElementById('banner');
+const loginButton = document.querySelector('.login button');
+
+signIn.addEventListener('click', (e) => {
+    e.preventDefault();
+    form.classList.remove('toggle');
+    banner.classList.remove('toggle')
+});
+
+signUp.addEventListener('click', (e) => {
+    e.preventDefault();
+    form.classList.add('toggle');
+    banner.classList.add('toggle');
+});
+
+loginButton.addEventListener('click', (e) => {
+    e.preventDefault(); // Evita que el formulario se envíe
+    window.location.href = 'dashboard.html'; // Redirige al dashboard
+});
+
